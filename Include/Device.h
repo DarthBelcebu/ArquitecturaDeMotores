4014872416810b379960ea3e31349b77691a9854
DarthBelcebu <114190257+DarthBelcebu@users.noreply.github.com>
2023-03-15 19:37:07 -0600

    Update Device.h

--- Include/Device.h
+++ Include/Device.h
@@ -61,8 +61,8 @@ public:
 
 	HRESULT
 	CreateBuffer(const D3D11_BUFFER_DESC* pDesc,
-               const D3D11_SUBRESOURCE_DATA* pInitialData,
-               ID3D11Buffer** ppBuffer);	
+		           const D3D11_SUBRESOURCE_DATA* pInitialData,
+		           ID3D11Buffer** ppBuffer);	
                //Creates a vertex buffer, index buffer, or shader constant buffer
 
 	HRESULT
